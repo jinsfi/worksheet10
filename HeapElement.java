90c929e463cfa27e3beaad94fbe8b5b48f5e9ef8
jinsfi <159089141+jinsfi@users.noreply.github.com>
2024-03-18 02:34:41 +0000

    Create HeapElement.java

---
+++ HeapElement.java
@@ -0,0 +1,13 @@
+public class HeapElement {
+
+    String str;
+
+    public HeapElement(String str) {
+        this.str = str;
+    }
+
+    public String toString() {
+        return this.str;
+    }
+
+}
